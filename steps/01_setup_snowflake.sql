ede1a0f2f31019c68cb55281958c038563376ee1
AnnaMgerian <annamgerian@gmail.com>
2025-11-01 17:24:51 -0700

    syntax fix

--- steps/01_setup_snowflake.sql
+++ steps/01_setup_snowflake.sql
@@ -12,8 +12,7 @@ CREATE OR ALTER DATABASE QUICKSTART_COMMON;
 -- API integration is needed for GitHub integration
 CREATE OR REPLACE API INTEGRATION git_api_integration
   API_PROVIDER = git_https_api
-  API_ALLOWED_PREFIXES = ('$PREFIX')
-
+  API_ALLOWED_PREFIXES = ('$REPO')
   ENABLED = TRUE;
 
  SHOW INTEGRATIONS;
@@ -25,8 +24,7 @@ CREATE OR REPLACE API INTEGRATION git_api_integration
 -- Git repository object is similar to external stage
 CREATE OR REPLACE GIT REPOSITORY quickstart_common.public.quickstart_repo
   API_INTEGRATION = git_api_integration
-  ORIGIN = '$REPO'
-
+  ORIGIN = '$REPO';
 
 CREATE OR ALTER DATABASE QUICKSTART_PROD;
 
